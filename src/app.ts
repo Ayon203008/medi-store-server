f4717404c07b57a422960c589ce24f4b9ef5aeaa
Ayon203008 <paypal572874@gmail.com>
2026-02-19 10:52:25 +0600

    better auth setup done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
+import { toNodeHandler } from "better-auth/node";
 import express, { Application, Request, Response } from "express";
-
+import { auth } from "./lib/auth";
 const app: Application = express();
 
-app.get("/",(req:Request,res:Response)=>{
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+app.use(express.json()) // * This will allow us to send data in the body of the request
+
+
+app.get("/", (req: Request, res: Response) => {
     res.send("Hello World")
 }) // * This will show the data in the website
 
